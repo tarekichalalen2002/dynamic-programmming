10daf33009b515948a0547e6f7a7f4978ed4da75
tarekichalalen2002 <t_ichalalen@estin.dz>
2023-08-29 23:18:10 +0200

    second commit

--- tabulation/count-construct.py
+++ tabulation/count-construct.py
@@ -1,2 +1,30 @@
 def count_construct(s , array) :
-    pass
+    tab = [0]*(len(s)+1)
+    tab[0] = 1
+
+    for i in range(len(s)+1):
+        if tab[i] > 0:
+            for a in array:
+                if i+len(a) <= len(s) and s[i:i+len(a)] == a:
+                    tab[i+len(a)] += tab[i]
+    return tab[len(s)]
+
+
+print(count_construct("abcdef", ["abc" , "ab" , "ef" , "def"]))
+print(count_construct("purple", ["purp" , "p" , "ur" , "le" , "purpl"]))
+print(count_construct("supercalifragilisticexpialidocious", ["super", "cali", "fragil", "istic", "ex", "piali", "doc", "ious", "sup", "exp"]))
+print(count_construct("abcdefghijklmno", ["ab", "cd", "ef", "gh", "ij", "kl", "mn", "op", "abc", "def", "ghi"]))
+print(count_construct("abracadabra", ["abra", "cad", "abrac", "ad", "abra", "br", "aca", "dab", "ra"]))
+
+print(count_construct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", 
+    [
+    "ee", 
+    "eee", 
+    "eeeee", 
+    "eeeeeeef", 
+    "eeeeeeeee",
+    "eeeeeeeeeee",
+    "eeeeeeeeeeeeeee",
+    "eeeeeeeeeeeeeeeeee", 
+]
+))
